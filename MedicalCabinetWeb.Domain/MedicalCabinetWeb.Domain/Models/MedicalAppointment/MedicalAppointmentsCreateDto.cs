c1d8d95a30f09278bdea8dfbd1a68901d4c6c20b
Malairau Dumitru <dumitru.malairau@isa.utm.md>
2026-03-22 19:26:13 +0200

    Creare Info si Create Dto pentru programare

---
+++ MedicalCabinetWeb.Domain/MedicalCabinetWeb.Domain/Models/MedicalAppointment/MedicalAppointmentsCreateDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MedicalCabinetWeb.Domain.Models.MedicalAppointment;
+
+public class MedicalAppointmentsCreateDto
+{
+    public string PatientName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string DoctorName { get; set; }
+    public string ServiceName { get; set; }
+    public string ReasonForVisit { get; set; }
+    public string AppointmentTime { get; set; } 
+    public DateTime AppointmentDate { get; set; }
+}
---
+++ MedicalCabinetWeb.Domain/MedicalCabinetWeb.Domain/Models/MedicalAppointment/MedicalAppointmentsInfoDto.cs
@@ -0,0 +1,17 @@
+﻿using MedicalCabinetWeb.Domain.Entities.MedicalAppointment;
+
+namespace MedicalCabinetWeb.Domain.Models.MedicalAppointment;
+
+public class MedicalAppointmentsInfoDto
+{
+    public int Id { get; set; }
+    public string PatientName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string DoctorName { get; set; }
+    public string ServiceName { get; set; }
+    public string ReasonForVisit { get; set; }
+    public string AppointmentTime { get; set; } 
+    public DateTime AppointmentDate { get; set; }
+    public AppointmentStatus Status { get; set; }
+}
