894063483fcf0ab0f64794bb1a60b7962f82f9e3
gudimavictor <victor.gudima@isa.utm.md>
2026-05-09 16:20:34 +0300

    Adaugare AllowAnonymous la AuthController

--- MedicalCabinetWeb.Api/Controllers/AuthController.cs
+++ MedicalCabinetWeb.Api/Controllers/AuthController.cs
@@ -1,12 +1,14 @@
 ﻿using MedicalCabinetWeb.BusinessLayer;
 using MedicalCabinetWeb.BusinessLayer.Interfaces;
 using MedicalCabinetWeb.Domain.Models.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedicalCabinetWeb.Api.Controllers;
 
 [Route("api/session")]
 [ApiController]
+[AllowAnonymous]
 public class AuthController : ControllerBase
 {
     private readonly IUserLoginLogic _userLogin;
