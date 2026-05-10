1d149bf73719fbd5662d68659c494f0553542545
Victor-Sirbu <victor.sirbu@isa.utm.md>
2026-05-10 16:20:17 +0300

    Authorize pentru RegisterController

--- MedicalCabinetWeb.Api/Controllers/RegisterController.cs
+++ MedicalCabinetWeb.Api/Controllers/RegisterController.cs
@@ -1,12 +1,14 @@
 ﻿using MedicalCabinetWeb.BusinessLayer;
 using MedicalCabinetWeb.BusinessLayer.Interfaces;
 using MedicalCabinetWeb.Domain.Models.User;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MedicalCabinetWeb.Api.Controllers;
 
 [Route("api/register")]
 [ApiController]
+[AllowAnonymous]
 public class RegisterController : ControllerBase
 {
     private readonly IUserRegLogic _userReg;
