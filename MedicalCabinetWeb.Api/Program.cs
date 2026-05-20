dd5699c71eb534d5a347f08dd6067cbc67abc4c1
malairaudumitru <dumitru.malairau@isa.utm.md>
2026-05-20 13:38:02 +0300

    Adaugare la cors noul link a serverului

--- MedicalCabinetWeb.Api/Program.cs
+++ MedicalCabinetWeb.Api/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173", "https://tweb-flax.vercel.app")
+            .WithOrigins("http://localhost:5173", "https://tweb-flax.vercel.app", "https://medicalcabinet.duckdns.org")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
