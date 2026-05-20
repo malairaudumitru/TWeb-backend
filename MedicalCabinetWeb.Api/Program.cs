ba50d2a632ac63345615e0c234282f77f1cc9106
malairaudumitru <dumitru.malairau@isa.utm.md>
2026-05-20 12:42:29 +0300

    Adaugare Vercel URL la CORS

--- MedicalCabinetWeb.Api/Program.cs
+++ MedicalCabinetWeb.Api/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins("http://localhost:5173", "https://tweb-flax.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
